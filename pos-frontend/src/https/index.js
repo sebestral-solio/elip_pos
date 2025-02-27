c6bac6bfbc17dd618c95122387498a3d626348a5
Amrit Raj <amrit.raj1504@gmail.com>
2025-02-27 19:40:22 +0530

    intial commit

---
+++ pos-frontend/src/https/index.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    }
+});
+
+// API Endpoints
+
+// Auth Endpoints
+export const login = (data) => api.post("/api/user/login", data);
+export const register = (data) => api.post("/api/user/register", data);
+export const getUserData = () => api.get("/api/user");
+export const logout = () => api.post("/api/user/logout");
+
+// Table Endpoints
+export const addTable = (data) => api.post("/api/table/", data);
+export const getTables = () => api.get("/api/table");
+export const updateTable = ({tableId, ...tableData}) => api.put(`/api/table/${tableId}`, tableData);
+
+
+// Payment Endpoints
+export const createOrderRazorpay = (data) => api.post("/api/payment/create-order", data);
+export const verifyPaymentRazorpay = (data) => api.post("/api/payment//verify-payment", data);
+
+
+// Order Endpoints
+export const addOrder = (data) => api.post("/api/order/", data);
+export const getOrders = () => api.get("/api/order");
+export const updateOrderStatus = ({orderId, orderStatus}) => api.put(`/api/order/${orderId}`, {orderStatus});
