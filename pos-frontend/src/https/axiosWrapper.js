d35fd7e2fd979e830bb3468a13fbd9af1d8cacb8
hamza_mustafa <hamza.mustafa@ellipsonic.com>
2025-07-13 10:56:31 +0530

    cors

--- pos-frontend/src/https/axiosWrapper.js
+++ pos-frontend/src/https/axiosWrapper.js
@@ -5,8 +5,34 @@ const defaultHeader = {
   Accept: "application/json",
 };
 
+// Create axios instance with custom configurations
 export const axiosWrapper = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
   headers: { ...defaultHeader },
+  timeout: 10000, // 10 seconds timeout
 });
+
+// Add a request interceptor
+axiosWrapper.interceptors.request.use(
+  (config) => {
+    console.log('Request:', config);
+    return config;
+  },
+  (error) => {
+    console.error('Request error:', error);
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axiosWrapper.interceptors.response.use(
+  (response) => {
+    console.log('Response:', response);
+    return response;
+  },
+  (error) => {
+    console.error('Response error:', error);
+    return Promise.reject(error);
+  }
+);
